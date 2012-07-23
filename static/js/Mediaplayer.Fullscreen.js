08f9e780cba31ebfaec126d6777c150513442ef8
Tim Benniks <tim@timbenniks.nl>
2012-07-23 18:10:35 +0200

    Fullscreen file file added

---
+++ static/js/Mediaplayer.Fullscreen.js
@@ -0,0 +1,21 @@
+/* 
+*	@Class:			Mediaplayer.Fullscreen
+*	@Description:	Makes teh player go fullscreen
+*	@Author:		Tim Benniks <tim.benniks@akqa.com>
+*	@Dependencies:	jQuery, Slider.js, Mediaplayer.Controlbar, Mediaplayer.Player
+---------------------------------------------------------------------------- */
+
+
+(function($, Mediaplayer)
+{
+	Mediaplayer.Fullscreen = function(actionElement, videoElement, options, playerInstance)
+	{
+		var init = function()
+		{
+			
+		};
+
+		init();
+	};
+
+})(jQuery, window.Mediaplayer = window.Mediaplayer || {});
